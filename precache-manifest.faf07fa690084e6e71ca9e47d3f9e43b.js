c0a21ccba400e87cd53c15ef2ac58983f16ade55
jk.shin <pooq99@gmail.com>
2019-06-21 17:57:31 +0900

    201906211757

---
+++ precache-manifest.faf07fa690084e6e71ca9e47d3f9e43b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c46408ca6a8a92082cf7b041a09256d",
+    "url": "https://stg.smartconsumer.kr/index.html"
+  },
+  {
+    "revision": "69453b705abea93db437",
+    "url": "https://stg.smartconsumer.kr/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "cbd0eed7212294cfc14d",
+    "url": "https://stg.smartconsumer.kr/static/css/main.6816c300.chunk.css"
+  },
+  {
+    "revision": "69453b705abea93db437",
+    "url": "https://stg.smartconsumer.kr/static/js/2.1205d0f0.chunk.js"
+  },
+  {
+    "revision": "cbd0eed7212294cfc14d",
+    "url": "https://stg.smartconsumer.kr/static/js/main.101613dd.chunk.js"
+  },
+  {
+    "revision": "2921ab725ba4465a8e1c",
+    "url": "https://stg.smartconsumer.kr/static/js/runtime~main.3526fdae.js"
+  }
+]);
